c143abb391c88d048b657632d221873aafbfddc2
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-23 16:20:52 -0500

    Allowed User Input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        //1. Allow user to input a number(guess)
+        //2. Tell the user if their guess is too high or too low.
+        //3. Allow for 5 chances, if the number isn't guessed, tell them what it is.
+        //4. If the number is guessed, congratulate them.
+        //5. Allow them to play again if they would like.
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Your Guess: ");
+        int userGuess = input.nextInt();
 
 
     }
