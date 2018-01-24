11a21ecbe7a81fcf2d7ad0f90a10a7afbd3a5322
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-24 16:09:33 -0500

    Guessing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,16 +12,31 @@ public class Main {
         //5. Allow them to play again if they would like.
 
         Scanner input = new Scanner(System.in);
+        boolean playAgain = true;
+        while (playAgain = true) {
+            System.out.println("Enter Your Guess: ");
+            int userGuess = input.nextInt();
 
-        System.out.println("Enter Your Guess: ");
-        int userGuess = input.nextInt();
+            int number = (int)Math.ceil(Math.random() * 100);
 
-        int number = (int)Math.round(Math.random());
+            if (userGuess > number) {
+                System.out.println("Your guess was too high, try again!");
+            }
+            if(userGuess < number) {
+                System.out.println("Your guess was too low, try again!");
+            }
+            else {
+                System.out.println("Congratulations!, you guessed correctly, Would you like to play again? 1 for Yes and 2 for No");
+                String playAgainTwo = input.nextLine();
+                if (playAgainTwo.equals("no")){
 
-        if (userGuess > number) {
-            System.out.println("Your guess was too high, try again!");
+                }
+                if (!playAgainTwo.equals("yes") || !playAgainTwo.equals("no")) {
+                    System.out.println("Ending Program due to invalid response");
+                    break;
+
+                }
+            }
         }
-        
-
     }
 }
