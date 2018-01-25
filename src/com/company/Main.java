02e3c7e13121da29b5d9f7fea8b88b97ed5bc60f
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-25 14:41:34 -0500

    Setting up the max guesses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,29 +12,51 @@ public class Main {
         //5. Allow them to play again if they would like.
 
         Scanner input = new Scanner(System.in);
+        int numberOfTimes = 0;
         boolean playAgain = true;
-        while (playAgain = true) {
+        int number = (int)Math.ceil(Math.random() * 100);
+        while (playAgain) {
             System.out.println("Enter Your Guess: ");
             int userGuess = input.nextInt();
 
-            int number = (int)Math.ceil(Math.random() * 100);
-
-            if (userGuess > number) {
-                System.out.println("Your guess was too high, try again!");
+            if (numberOfTimes >= 5){
+                System.out.println("Sorry! Would you like to play again?");
+                String playAgainThree = input.nextLine();
+                if (playAgainThree.equals("no")){
+                    System.out.println("Have a nice day!");
+                    playAgain = false;
+                }
+                else if (!playAgainThree.equals("yes") || !playAgainThree.equals("no")) {
+                    System.out.println("Ending Program due to invalid response");
+                    playAgain = false;
+                }
+                else if (playAgainThree.equals("yes")){
+                    System.out.println("Great!");
+                    playAgain = true;
             }
-            if(userGuess < number) {
+
+            else if (userGuess > number) {
+                System.out.println("Your guess was too high, try again!");
+                numberOfTimes = numberOfTimes + 1;
+            }
+            else if(userGuess < number) {
                 System.out.println("Your guess was too low, try again!");
+                numberOfTimes = numberOfTimes + 1;
             }
             else {
-                System.out.println("Congratulations!, you guessed correctly, Would you like to play again? 1 for Yes and 2 for No");
+                System.out.println("Congratulations!, you guessed correctly, Would you like to play again?");
                 String playAgainTwo = input.nextLine();
                 if (playAgainTwo.equals("no")){
-
+                    System.out.println("Have a nice day!");
+                    playAgain = false;
                 }
-                if (!playAgainTwo.equals("yes") || !playAgainTwo.equals("no")) {
+                else if (!playAgainTwo.equals("yes") || !playAgainTwo.equals("no")) {
                     System.out.println("Ending Program due to invalid response");
-                    break;
-
+                    playAgain = false;
+                }
+                else if (playAgainTwo.equals("yes")){
+                    System.out.println("Great!");
+                    playAgain = true;
                 }
             }
         }
