d3f717e6bce0bc66721551417755615e8a3c5861
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-30 09:55:37 -0500

    Method changes

--- src/com/company/GuessingGame.java
+++ src/com/company/GuessingGame.java
@@ -1,3 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void game() {
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
 
 
 
