67e99c77633528fa5a48d1a0ddce5119ddb8ce3b
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-30 16:28:10 -0500

    Fully complete game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,74 +5,80 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //1. Allow user to input a number(guess)
-        //2. Tell the user if their guess is too high or too low.
-        //3. Allow for 5 chances, if the number isn't guessed, tell them what it is.
-        //4. If the number is guessed, congratulate them.
-        //5. Allow them to play again if they would like.
+        Main Game = new Main();
+        Game.game();
+    }
 
-        Scanner input = new Scanner(System.in);
-        Scanner yesNo = new Scanner(System.in);
-        int tries = 0;
-        int wins = 0;
-        int losses = 0;
-        int numberOfTimes = 0;
-        boolean playAgain = true;
+    Scanner yesNo = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
+    int number = (int) (Math.ceil(Math.random() * 100));
+    int tries = 0;
+    int wins = 0;
+    int losses = 0;
+    int numberOfTimes = 0;
+    boolean playAgain = true;
+
+
+    public void game() {
+        // number of times a user has guessed and if they want to keep playing
         Utility.instructions();
-        int number = (int)(Math.ceil(Math.random() * 100));
+
         while (playAgain) {
             Utility.guess();
+            // catching for any non-number input
             try {
                 int userGuess = input.nextInt();
                 if (userGuess > number) {
                     Utility.lower();
-                    numberOfTimes +=  1;
-                } else if(userGuess < number) {
+                    numberOfTimes += 1;
+                } else if (userGuess < number) {
                     Utility.higher();
                     numberOfTimes += 1;
-                }else if(userGuess == number){
-                    tries += 1;
-                    wins += 1;
-                     Utility.Win(tries,wins,losses);
+                } else if (userGuess == number) { //for replaying if the user guesses correctly
+                    wins ++;
+                    tries ++;
+                    Utility.Win(tries, wins, losses);
                     String playAgainTwo = yesNo.nextLine();
                     number = (int) (Math.ceil(Math.random() * 100));
                     if (playAgainTwo.equals("N") || playAgainTwo.equals("n")) {
                         Utility.no();
-
                         playAgain = false;
                     } else if (playAgainTwo.equals("Y") || playAgainTwo.equals("y")) {
                         Utility.yes();
                         playAgain = true;
                         numberOfTimes = 0;
                     } else {
-                        Utility.invalidGuess();
+                        Utility.invalid();
                         playAgain = false;
                     }
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
+                input.nextLine();
                 Utility.invalidGuess();
-                break;
+                numberOfTimes = 0;
             }
-            if (numberOfTimes >= 5) {
-                tries += 1;
-                losses += 1;
-                String playAgainThree = " ";
-                Utility.notEquals(number, tries, wins, losses);
-                playAgainThree = yesNo.nextLine();
-                number = (int)(Math.ceil(Math.random() * 100));
-                if (playAgainThree.equals("N") || playAgainThree.equals("n")) {
-                    Utility.no();
-                    playAgain = false;
-                } else if (playAgainThree.equals("Y") || playAgainThree.equals("y")) {
-                    Utility.yes();
-                    playAgain = true;
-                    numberOfTimes = 0;
-                } else {
-                    Utility.invalidGuess();
-                    playAgain = false;
-                }
+            if (numberOfTimes >= 5){
+                replay();
             }
+        }
+    }
+    public void replay(){
+        String playAgainThree = "";
+        losses ++;
+        tries++;
+        Utility.notEquals(number, tries, wins, losses);
+        playAgainThree = yesNo.nextLine();
+        number = (int) (Math.ceil(Math.random() * 100));
+        if (playAgainThree.equals("N") || playAgainThree.equals("n")) {
+            Utility.no();
+            playAgain = false;
+        } else if (playAgainThree.equals("Y") || playAgainThree.equals("y")) {
+            Utility.yes();
+            playAgain = true;
+            numberOfTimes = 0;
+        } else {
+            Utility.invalidGuess();
+            playAgain = false;
         }
     }
 }
--- src/com/company/Utility.java
+++ src/com/company/Utility.java
@@ -14,10 +14,7 @@ public class Utility {
         System.out.println("This is not a number. \n");
     }
     public static void instructions(){
-        System.out.println("Guess a number between 1 and 100, you have 5 guesses. \n");
-    }
-    public static void restart(){
-        System.out.println("Would you like to play again? Y/N. \n");
+        System.out.println("Welcome to the Guessing Game! \nGuess a number between 1 and 100, you have 5 guesses. \n");
     }
     public static void guess(){
         System.out.print("Enter your guess: \n");
@@ -30,6 +27,9 @@ public class Utility {
     }
     public static void notEquals(int number, int tries, int wins ,int losses) {
         System.out.println("Sorry! The answer was " + number + ". You have played " + tries + " times, " + wins + " wins, " + losses + " losses. Would you like to play again? Y/N. \n");
+    }
+    public static void invalid(){
+        System.out.println("That isn't a yes, so im going to take it as a no. Have a nice day!");
     }
 
 }
